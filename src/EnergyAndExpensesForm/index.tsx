15624f4b29f19f8457a3bbc15a7d12f6045d90cd
Gasyh3 <kevrakt@gmail.com>
2023-04-14 18:47:08 +0200

    1er etape : Création d'un formulaire en react

--- src/EnergyAndExpensesForm/index.tsx
+++ src/EnergyAndExpensesForm/index.tsx
@@ -1,1 +1,230 @@
-export const EnergyAndExpensesForm = () => <p>Zone de travail</p>;
+import React, { useState } from 'react';
+import { useForm, SubmitHandler, useFieldArray  } from 'react-hook-form';
+import {
+    TextField,
+    FormControl,
+    InputLabel,
+    Select,
+    MenuItem,
+    Button,
+    Box,
+    Typography,
+  } from '@mui/material';
+
+  interface ChaufferieData {
+    natureOfEcsProduction: string;
+    yearsData: {
+      id: number;
+      startConsumptionPeriod: string;
+      endConsumptionPeriod: string;
+      currentEnergy: string;
+      currentEnergyEcs?: string;
+      totalConsumption?: number;
+      costConsumption?: number;
+      costEcs?: number;
+      volumeEcs?: number;
+      maintenanceCost?: number;
+      renovationCost?: number;
+      dju: number;
+    }[];
+  }
+
+export const EnergyAndExpensesForm = () => {
+    const { register, handleSubmit, control, getValues, watch } = useForm<ChaufferieData>();
+    const [yearsData, setYearsData] = useState([{ id: 0 }]);
+    const natureOfEcsProduction = watch('natureOfEcsProduction');
+  
+    const onSubmit: SubmitHandler<ChaufferieData> = (data) => {
+      console.log(data);
+    };
+
+
+
+    
+    //Pour le bonus 1 :
+    const addYear = () => {
+        
+      if (yearsData.length < 5) {
+        setYearsData([...yearsData, { id: yearsData.length }]);
+      }
+    };
+
+  
+
+    const removeYear = (id: number) => {
+      setYearsData(yearsData.filter((year) => year.id !== id));
+    };
+
+    return (
+
+    <form onSubmit={handleSubmit(onSubmit)}>
+    <Box>
+      <FormControl fullWidth variant="outlined" margin="normal">
+        <InputLabel htmlFor="natureOfEcsProduction">Nature de la production ECS</InputLabel>
+        <Select
+          {...register('natureOfEcsProduction')}
+          id="natureOfEcsProduction"
+          label="Nature de la production ECS"
+        >
+          <MenuItem value="shared">Partagée</MenuItem>
+          <MenuItem value="separated">Séparée</MenuItem>
+          <MenuItem value="individual">Individuelle</MenuItem>
+        </Select>
+      </FormControl>
+    </Box>
+
+    {yearsData.map((year) => (
+      <Box key={year.id}>
+        <Typography variant="h6">Année {year.id + 1}</Typography>
+        <Box display="flex" flexDirection="row" justifyContent="space-between">
+          <TextField
+            {...register(`yearsData.${year.id}.startConsumptionPeriod`)}
+            id={`startConsumptionPeriod-${year.id}`}
+            label="Début de la période"
+            type="date"
+            sx={{ width: '49%' }}
+            InputLabelProps={{
+              shrink: true,
+            }}
+          />
+          <TextField
+            {...register(`yearsData.${year.id}.endConsumptionPeriod`)}
+            id={`endConsumptionPeriod-${year.id}`}
+            label="Fin de la période"
+            type="date"
+            sx={{ width: '49%' }}
+            InputLabelProps={{                shrink: true,
+            }}
+          />
+        </Box>
+
+        <TextField
+          {...register(`yearsData.${year.id}.currentEnergy`)}
+          id={`currentEnergy-${year.id}`}
+          label="Type d'énergie"
+          select
+          fullWidth
+          variant="outlined"
+          margin="normal"
+        >
+          <MenuItem value="naturalGas">Gaz naturel</MenuItem>
+          <MenuItem value="oil">Fioul</MenuItem>
+          <MenuItem value="electricity">Électricité</MenuItem>
+          <MenuItem value="woodpellets">Granulés de bois</MenuItem>
+        </TextField>
+
+        {natureOfEcsProduction === 'separated' && (
+          <TextField
+            {...register(`yearsData.${year.id}.currentEnergyEcs`)}
+            id={`currentEnergyEcs-${year.id}`}
+            label="Type d'énergie ECS"
+            select
+            fullWidth
+            variant="outlined"
+            margin="normal"
+          >
+            <MenuItem value="naturalGas">Gaz naturel</MenuItem>
+            <MenuItem value="oil">Fioul</MenuItem>
+            <MenuItem value="electricity">Électricité</MenuItem>
+            <MenuItem value="woodpellets">Granulés de bois</MenuItem>
+          </TextField>
+        )}
+
+        {natureOfEcsProduction !== 'separated' && (
+          <>
+            <TextField
+              {...register(`yearsData.${year.id}.totalConsumption`)}
+              id={`totalConsumption-${year.id}`}
+              label="Consommation totale"
+              type="number"
+              fullWidth
+              variant="outlined"
+              margin="normal"
+            />
+            <TextField
+              {...register(`yearsData.${year.id}.costConsumption`)}
+              id={`costConsumption-${year.id}`}
+              label="Coût énergétique total"
+              type="number"
+              fullWidth
+              variant="outlined"
+              margin="normal"
+            />
+          </>
+        )}
+
+        {['shared', 'separated'].includes(natureOfEcsProduction) && (
+          <>
+            <TextField
+              {...register(`yearsData.${year.id}.costEcs`)}
+              id={`costEcs-${year.id}`}
+              label="Coût ECS"
+              type="number"
+              fullWidth
+              variant="outlined"
+              margin="normal"
+            />
+            <TextField
+              {...register(`yearsData.${year.id}.volumeEcs`)}
+              id={`volumeEcs-${year.id}`}
+              label="Volume ECS"
+              type="number"
+              fullWidth
+              variant="outlined"
+              margin="normal"
+            />
+          </>
+        )}
+
+        <TextField
+          {...register(`yearsData.${year.id}.maintenanceCost`)}
+          id={`maintenanceCost-${year.id}`}
+          label="Coût maintenance chaufferie"
+          type="number"
+          fullWidth
+          variant="outlined"
+          margin="normal"
+        />
+        <TextField
+          {...register(`yearsData.${year.id}.renovationCost`)}
+          id={`renovationCost-${year.id}`}
+          label="Coût travaux chaufferie"
+          type="number"
+          fullWidth
+          variant="outlined"
+          margin="normal"
+        />
+        <TextField
+          {...register(`yearsData.${year.id}.dju`)}
+          id={`dju-${year.id}`}
+          label="DJU"
+          type="number"
+          fullWidth
+          variant="outlined"
+          margin="normal"
+        />
+
+        {year.id !== 0 && (
+          <Button variant="contained" color="secondary" onClick={() => removeYear(year.id)}>
+            Supprimer l'année
+          </Button>
+        )}
+              </Box>
+      ))}
+
+      <Box my={2}>
+        {yearsData.length < 5 && (
+          <Button variant="contained" color="primary" onClick={addYear}>
+            Ajouter une année
+          </Button>
+        )}
+      </Box>
+
+      <Box my={2}>
+        <Button variant="contained" color="primary" type="submit">
+          Enregistrer
+        </Button>
+      </Box>
+    </form>
+  );
+};
